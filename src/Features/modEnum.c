41045f42098a84470a6f28e86a390c9ed1936fb1
Aspasia1337 <thereald3al@proton.me>
2025-10-09 19:59:04 +0200

    System module enumeration

---
+++ src/Features/modEnum.c
@@ -0,0 +1,37 @@
+#include "modEnum.h"
+
+void moduleEnumeration(void) {
+	NTSTATUS status;
+	ULONG size = 0;
+	PVOID buffer = 0;
+
+	status = ZwQuerySystemInformation(SystemModuleInformation, 0, 0, &size);
+	
+	// Trick for getting the size of the buffer
+
+	if(status != STATUS_INFO_LENGTH_MISMATCH) {
+		SendMessage("Error getting the size of the buffer in ZwQuerySystemInformation\n");
+		return;
+	}
+
+	buffer = ExAllocatePool2(POOL_FLAG_NON_PAGED, size, 'buff');
+
+	status = ZwQuerySystemInformation(SystemModuleInformation, buffer, size, &size);
+
+	if(!NT_SUCCESS(status)){
+		SendMessage("Error getting System Information from the ZwQuerySystemInformation call\n");
+		ExFreePoolWithTag(buffer, 'buff');
+		return;
+	}
+
+	PSYSTEM_MODULE_INFORMATION modInfo = (PSYSTEM_MODULE_INFORMATION)buffer;
+	PSYSTEM_MODULE_ENTRY mod;
+
+	for(ULONG i = 0; i < modInfo->Count; i++) {
+		mod = &modInfo->Module[i];
+		const char* name = (const char*)(mod->FullPathName + mod->OffsetToFileName);
+		SendMessage(name);
+	}
+
+	ExFreePoolWithTag(buffer, 'buff');
+}
---
+++ src/Features/modEnum.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "../common.h"
+#include "../utils.h"
+
+void moduleEnumeration(void);
+
+
