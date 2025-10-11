3aff5dbb53908ab332e21ba055e4770c69f74648
Aspasia1337 <thereald3al@proton.me>
2025-10-11 20:02:19 +0200

    Message queue

---
+++ src/Queue/queue.c
@@ -0,0 +1,61 @@
+﻿#include "queue.h"
+
+MESSAGE_QUEUE gQueue;
+KSPIN_LOCK gQueueLock;
+BOOLEAN gQueueInitialized;
+
+void InitQueue(void) {
+	KIRQL IRQ;
+	if (!gQueueInitialized) {
+		KeInitializeSpinLock(&gQueueLock);
+		KeAcquireSpinLock(&gQueueLock, &IRQ);
+		gQueueInitialized = 1;
+		gQueue.Count = 0;
+		gQueue.ReadIndex = 0;
+		gQueue.WriteIndex = 0;
+		KeReleaseSpinLock(&gQueueLock, IRQ);
+	}
+}
+
+BOOLEAN EnqueueMessage(const char* message) {
+	KIRQL IRQ;
+	
+	KeAcquireSpinLock(&gQueueLock, &IRQ);
+
+	if (!gQueueInitialized)
+		return 0;
+
+	if (gQueue.Count >= MAXMSG) {
+		gQueue.ReadIndex = (gQueue.ReadIndex + 1) % MAXMSG;
+	}
+	else {
+		gQueue.Count += 1;
+	}
+
+	RtlStringCchCopyA(gQueue.Messages[gQueue.WriteIndex], MAXLEN, message);
+	gQueue.WriteIndex = (gQueue.WriteIndex + 1) % MAXMSG;
+
+	KeReleaseSpinLock(&gQueueLock, IRQ);
+	return 1;
+}
+
+BOOLEAN DequeueMessage(char* outbuffer, ULONG bufferSize) {
+	KIRQL IRQ;
+
+	KeAcquireSpinLock(&gQueueLock, &IRQ);
+
+	if (!gQueueInitialized)
+		return 0;
+
+	if (gQueue.Count == 0) {
+		return 0;
+	}
+
+	RtlStringCchCopyA(outbuffer, bufferSize, gQueue.Messages[gQueue.ReadIndex]);
+
+	gQueue.ReadIndex = (gQueue.ReadIndex + 1) % MAXMSG;
+	gQueue.Count -= 1;
+
+	KeReleaseSpinLock(&gQueueLock, IRQ);
+	return 1;
+}
---
+++ src/Queue/queue.h
@@ -0,0 +1,20 @@
+#include <ntddk.h>
+#include <ntstrsafe.h>
+
+#define MAXMSG 100
+#define MAXLEN 256
+
+typedef struct _MESSAGE_QUEUE {
+	char Messages[MAXMSG][MAXLEN];
+	ULONG WriteIndex; 
+	ULONG ReadIndex; 
+	ULONG Count;
+} MESSAGE_QUEUE;
+
+extern MESSAGE_QUEUE gQueue;
+extern KSPIN_LOCK gQueueLock;
+extern BOOLEAN gQueueInitialized;
+
+void InitMessageQueue(void);
+BOOLEAN EnqueueMessage(const char*);
+BOOLEAN DequeueMessage(char* outbuffer, ULONG bufferSize);
